52d2b3815e0f8b3c3a2325c02453679bd3b4434c
Jakub Ślusarski <s20568@pjwstk.edu.pl>
2022-06-12 13:44:29 +0200

    init

---
+++ Kolokwium1/Kolokwium1/Models/Album.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kolokwium1.Models
+{
+    public class Album
+    {
+        [Key]
+        public int IdAlbum { get; set; }
+        
+        [Required]
+        [MaxLength(30)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string LastName { get; set; }
+    }
+}
